1f4f23e8afaccb8d09627b59a24fa01ec6cd761e
Arpana <arpanageorge@yahoo.in>
2019-04-29 10:35:30 -0700

    Updates

---
+++ precache-manifest.7e31b2755242c96f0876602bd4ad77ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a108fede9253203a15bc59109c01136a",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "d0f8b519a5808d9e9b19",
+    "url": "/ClickyGame/static/css/main.dde50f4a.chunk.css"
+  },
+  {
+    "revision": "48bb99afeae4b3f345e6",
+    "url": "/ClickyGame/static/js/2.3c1fd096.chunk.js"
+  },
+  {
+    "revision": "d0f8b519a5808d9e9b19",
+    "url": "/ClickyGame/static/js/main.3bc80fa3.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
